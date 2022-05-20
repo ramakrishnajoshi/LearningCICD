15e9b4147f57b261338fb3415e0819a46cf60d04
Ramakrishna Joshi <rjoshi@falabella.cl>
2022-05-21 00:32:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/learningcicd/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.learningcicd
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.learningcicd.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
